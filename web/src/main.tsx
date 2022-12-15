904f5a12235462388ffc01e7875fd9af1103687a
Upenha <contato.upenha@gmail.com>
2022-12-14 22:53:37 -0300

    eu vou cometer suicidio puta merda eu me odeio

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Router } from './router/router'
 import { GlobalStyle } from './styles/globalStyle'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
     <GlobalStyle />
   </React.StrictMode>,
 )
